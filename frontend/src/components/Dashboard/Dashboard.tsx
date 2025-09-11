479e4ef31917e9d45af64d935bd9391affdf0d4e
Gianpierre Terry <gterry@unomaha.edu>
2025-09-10 21:09:53 -0500

    feature: added barebones dashboard template

---
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from '../Card/Card.tsx';
+
+import '../Card/Card.css'
+
+export default function Dashboard() {
+    return(
+       <div>
+          <Card>
+            <div></div>
+          </Card>
+
+
+       </div>
+    )
+}
