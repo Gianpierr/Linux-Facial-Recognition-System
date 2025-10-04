2a089bfee94f58a2154b6b0634b19b5d18e4c04b
Gianpierre Terry <gterry@unomaha.edu>
2025-10-04 17:56:15 -0500

    feat: created card component for dashboard

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -4,13 +4,30 @@ import React from 'react';
 interface CardProps {
     children: React.ReactNode;
     className ?: string;
+    key: string;
+    title: string;
+    image: string;
+    size: string;
+
 }
 
-export default function Card( {children, className = ""}: CardProps) {
+export default function Card( { children, title, image, size = "normal", className = ""}: CardProps) {
     return(
-        <div className={`modern-card ${className}`}>
-            {children}
-        </div>
+        
+            <div className={`card ${size} ${className}`}>
+                { image && (
+                    <div className="card-image">
+                        <img></img>
+                    </div>
+                )}
+                <div className="card-content">
+                    <h3 className="card-title">{title}</h3>
+                    {children}
+
+                </div>
+            </div>
+    
+
 
     );
 }
