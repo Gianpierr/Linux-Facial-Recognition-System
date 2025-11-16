956df87551d659d84e83e3890dd214ed750e9aac
Gianpierre Terry <gterry@unomaha.edu>
2025-11-16 14:27:32 -0600

    - Created user view with GET and POST endpoint handlers - Added pagination for user list

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,55 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
+from .models import (
+    User
+)
+from .serializers import (
+    UserSerializer
+)
 
-def home(request):
+class UserPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class UserView(APIView):
+
+    def get_user(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return None
     
-    return HttpResponse("Hello world!")
+    def get(self, request, pk=None):
+        if pk:
+            try:
+                user = self.get_user(pk=pk)
+                serializer = UserSerializer(user)
+                return Response(serializer.data)
+            except User.DoesNotExist:
+                return Response(
+                {"error": "User not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        else:
+            users = User.objects.all()
+
+            paginator = UserPagination()
+            paginated_users = paginator.paginate_queryset(users, request)
+
+            serializer = UserSerializer(paginated_users, many=True)
+
+            return paginator.get_paginated_response(serializer.data)
+        
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
