4fbbbcc443553dfc01c4e5114922ccf7d4e8fc9c
Gianpierre Terry <gterry@unomaha.edu>
2025-11-17 10:51:37 -0600

    Created ProfileSerializer to allow users to update their profile pictures

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,28 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-# TODO: Change to Model Serializer 
+from models import (
+    UserProfile
+)
+
+# TODO: Change to Model Serializer
+
+class UserProfileSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    phone = serializers.CharField(max_length=15)
+    profile_pic = serializers.ImageField()
+
+    def create(self, validated_data):
+        return UserProfile.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items(): # iterate through update fields and set
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
+    
+
+
 class UserSerializer(serializers.Serializer):
     """ Serializer for user """
     id = serializers.IntegerField(read_only=True)
@@ -8,15 +30,18 @@ class UserSerializer(serializers.Serializer):
     email = serializers.EmailField()
     first_name = serializers.CharField(max_length=150, required=False)
     last_name = serializers.CharField(max_length=150, required=False)
+    password = serializers.CharField(write_only=True, min_length=8)
+    profile = UserProfileSerializer(required=False) 
+    
 
     def create(self, validated_data):
         return User.objects.create(**validated_data)
     
     def update(self, instance, validated_data):
-        instance.username = validated_data.get("username", instance.username)
-        instance.email = validated_data.get("email", instance.email)
-        instance.first_name = validated_data.get("first_name", instance.first_name)
-        instance.last_name = validated_data.get("last_name", instance.last_name)
+        validated_data.pop("password", None ) # Remove password if present
+        for attr, value in validated_data.items(): # iterate through update fields and set
+            setattr(instance, attr, value)
+
         instance.save()
         return instance
     
