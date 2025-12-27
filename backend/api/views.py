b0958d156b9383873b2320b14e123e241d67155f
Gianpierre Terry <gterry@unomaha.edu>
2025-12-27 16:59:13 -0600

    Implemented the following:  - Added View for Photo and Notification model  - Added ViewSet (read-only) for Event and Detection models

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,13 +3,22 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.pagination import PageNumberPagination
+from rest_framework import viewsets
 from .models import (
     User,
     Video,
+    Photo,
+    Notification,
+    Detection,
+    Event
 )
 from .serializers import (
     UserSerializer,
     VideoSerializer,
+    PhotoSerializer,
+    NotificationSerializer,
+    DetectionSerializer,
+    EventSerializer
 )
 
 class UserPagination(PageNumberPagination):
@@ -22,18 +31,27 @@ class VideoPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 100
 
-class UserView(APIView):
+class PhotoPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
-    def get_user(self, pk):
-        try:
-            return User.objects.get(pk=pk)
-        except User.DoesNotExist:
-            return None
+class NotificationPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+class DetectionPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class UserView(APIView):
     
     def get(self, request, pk=None):
         if pk:
             try:
-                user = self.get_user(pk=pk)
+                user = User.objects.get(id = pk)
                 serializer = UserSerializer(user)
                 return Response(serializer.data)
             except User.DoesNotExist:
@@ -43,10 +61,8 @@ class UserView(APIView):
             )
         else:
             users = User.objects.all()
-
             paginator = UserPagination()
             paginated_users = paginator.paginate_queryset(users, request)
-
             serializer = UserSerializer(paginated_users, many=True)
 
             return paginator.get_paginated_response(serializer.data)
@@ -90,7 +106,7 @@ class UserView(APIView):
                 status=status.HTTP_404_NOT_FOUND
             )
         try:
-            user = User.objects.get(pk=pk).delete()
+            User.objects.get(pk=pk).delete()
             
             return Response(
                 status=status.HTTP_204_NO_CONTENT
@@ -103,7 +119,6 @@ class UserView(APIView):
             
 
 class VideoView(APIView):
-    
     def get(self, request, pk = None):
         if pk:
             try:
@@ -120,7 +135,7 @@ class VideoView(APIView):
             videos = Video.objects.all()
             paginator = VideoPagination()
             paginated_videos = paginator.paginate_queryset(videos, request)
-            serializer = VideoSerializer(paginated_videos, many=True)
+            serializer = VideoSerializer(paginated_videos, many = True)
 
             return paginator.get_paginated_response(serializer.data)
     
@@ -134,6 +149,10 @@ class VideoView(APIView):
                     serializer.data, 
                     status = status.HTTP_201_CREATED
                 )
+        return Response(
+            serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST
+        )
     
     def put(self, request, pk = None):
         if not pk:
@@ -145,7 +164,7 @@ class VideoView(APIView):
         try:
             video = Video.objects.get(id = pk)
 
-            serializer = VideoSerializer(video, data = request.data, many = True)
+            serializer = VideoSerializer(video, data = request.data, partial = True)
 
             if serializer.is_valid():
                 serializer.save()
@@ -171,18 +190,202 @@ class VideoView(APIView):
                 {"error": "VideoID required for deletion."}
             )
         try:
-            video = Video.objects.get(id = pk).delete()
-
+            Video.objects.get(id = pk).delete()
             return Response(
                 status = status.HTTP_204_NO_CONTENT
             )
 
         except Video.DoesNotExist:
-
             return Response(
                 {"error": "Video not found."},
                 status = status.HTTP_404_NOT_FOUND
             )
+        
+class PhotoView(APIView):
+    def get(self, request, pk = None):
+        if pk:
+            try:
+                photo = Photo.objects.get(id = pk)
+                serializer = PhotoSerializer(photo)
+                return Response(
+                    serializer.data,
+                    status = status.HTTP_200_OK
+                )
+
+            except Photo.DoesNotExist:
+                return Response(
+                    {"error": "Photo not found."},
+                    status = status.HTTP_404_NOT_FOUND
+                ) 
+        
+        photos = Photo.objects.all()
+
+        paginator = PhotoPagination()
+
+        paginated_photos = paginator.paginate_queryset(photos, request)
+
+        serializer = PhotoSerializer(paginated_photos, many = True)
+
+        return paginator.get_paginated_response(serializer.data)
+    
+    def post(self, request):
+        serializer = PhotoSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status = status.HTTP_201_CREATED
+            )
+        
+        return Response(
+            serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST
+        )
+
+    def put(self, request, pk = None):
+        if not pk:
+            return Response(
+                {"error": "PhotoID required for update."},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        try:
+            photo = Photo.objects.get(id = pk)
+            serializer = PhotoSerializer(photo, request.data, partial = True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    serializer.data,
+                    status = status.HTTP_200_OK
+                )
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        except Photo.DoesNotExist:
+            return Response(
+                {"error": "PhotoID required for update."}
+            )
+        
+    def delete(self, request, pk = None):
+        if not pk:
+            return Response(
+                {"error": "PhotoID not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        try:
+            Photo.objects.get(pk=pk).delete()
+            return Response(
+                status=status.HTTP_204_NO_CONTENT
+            )
+        except Photo.DoesNotExist:
+            return Response(
+                {"error": "User not found."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+class NotificationView(APIView):
+    def get(self, request, pk = None):
+        if not pk:
+            notifications = Notification.objects.all()
+            paginator = NotificationPagination()
+            paginated_notifs = paginator.paginate_queryset(notifications, request)
+            serializer = NotificationSerializer(paginated_notifs, many = True)
+
+            return paginator.get_paginated_response(serializer.data)
+
+        try:
+            notification = Notification.objects.get(id = pk)
+            serializer = NotificationSerializer(notification)
+            return Response(
+                serializer.data,
+                status = status.HTTP_200_OK
+            )
+        except Notification.DoesNotExist:
+            return Response(
+                {"error": "Notification not found."},
+                status = status.HTTP_404_NOT_FOUND
+            )
+    
+    def post(self, request):
+        serializer = NotificationSerializer(request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data, 
+                status = status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST
+        )
+    
+    def put(self, request, pk = None ):
+        
+        if not pk:
+            return Response(
+                {"error": "NotificationID required for update."},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        
+        try:
+            notification = Notification.objects.get(id = pk)
+            serializer = NotificationSerializer(instance = notification, data = request.data, partial = True)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    serializer.data,
+                    status = status.HTTP_200_OK
+                )
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+
+        except Notification.DoesNotExist:
+            
+            return Response(
+                {"error": "NotificationID required for update."},
+                status = status.HTTP_404_NOT_FOUND
+            )
+        
+    def delete(self, request, pk = None):
+        if not pk:
+            return Response(
+                {"error": "NotificationID required for deletion"},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+
+            Notification.objects.get(id = pk).delete()
+            return Response(
+                status = status.HTTP_204_NO_CONTENT
+            )
+        except Notification.DoesNotExist:
+            return Response(
+                {"error": "NotificationID required for deletion."},
+                status = status.HTTP_404_NOT_FOUND
+            )
+
+
+class DetectionViewSet(viewsets.ReadOnlyModelViewSet):
+    """ 
+    Read-only API for detections (created by AI processessing , not users)
+    Provides: list() and retreive() only
+    """
+    queryset = Detection.objects.all()
+    serializer_class = DetectionSerializer
+
+class EventViewSet(viewsets.ReadOnlyModelViewSet):
+
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+
 
 
 
