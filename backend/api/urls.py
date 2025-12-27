187aedbdf223fba444ea27fbf23e58eeb8f0c4cd
Gianpierre Terry <gterry@unomaha.edu>
2025-12-27 16:57:38 -0600

    Added endpoints for all Views and ViewSets

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,26 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import (
     UserView, 
     VideoView,
+    PhotoView,
+    NotificationView,
+    DetectionViewSet
 )
 
+router = DefaultRouter()
+router.register(r'detections', DetectionViewSet, basename = 'detection')
 
 urlpatterns = [
-    path('users/<int:pk>/', UserView.as_view(), name = "users"),
-    path("videos/<int:pk>/", VideoView.as_view(), name = "videos")
+    path('users/', UserView.as_view(), name = "users"),
+    path('users/<int:pk>/', UserView.as_view(), name = "users-detail"),
+    path("videos/", VideoView.as_view(), name = "videos"),
+    path("videos/<int:pk>/", VideoView.as_view(), name = "videos-detail"),
+    path("photos/", PhotoView.as_view(), name = "photos"),
+    path("photos/<int:pk>/", PhotoView.as_view(), name = "photos"),
+    path("notifications/", NotificationView.as_view(), name = "notifications-detail"),
+    path("notifications/<int:pk>/", NotificationView.as_view(), name = "notifications-detail"),
+    # path("detections/", DetectionViewSet.as_view({"get":"list"}), name = "detections"),
+    # path("detections/<int:pk>", DetectionViewSet.as_view({"get":"retrieve"}), name = "detections-detail"),
+   path('', include(router.urls))
 ]
